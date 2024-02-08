71a935a3cc23c33c96190fb43f27940e5cb43114
jeminsong <143300705+jeminsong@users.noreply.github.com>
2024-02-07 18:51:50 -0800

    Update main.cc

--- prob02/main.cc
+++ prob02/main.cc
@@ -13,31 +13,24 @@
 
 #include "salary.h"
 using namespace std;
+
 int main() {
   double hourly_wages = 0;
   double hours_worked = 0;
-  While (true) {
-      cout << "Hourly wage: ";
-      cin >> hourly_wages;
-      if (hourly_wages < 0) {
-          cout << "Negative hourly wage is invalid.\n";
-          continue;
-      }
-      break;
-  }
-  While (true) {
-      cout << "Hours worked: ";
-      cin >> hours_worked;
-      if (hours_worked < 0) {
-          cout << "Negative hours worked is invalid.\n";
-          continue;
-      }
-      break;
-  }
-
+  cout << "Hourly wage: ";
+  cin >> hourly_wages;
+  cout << "Hours worked: ";
+  cin >> hours_worked;
   double salary = ComputeSalary(hourly_wages, hours_worked);
-
-  cout << std::fixed << std::setprecision(2) << "Total Salary Owed: $"
+  if (hourly_wages < 0) {
+    cout << "Negative hourly wage is invalid.\n";
+    return -1;
+  }
+  if (hours_worked < 0) {
+    cout << "Negative hours worked is invalid.\n";
+    return -1;
+  }
+  std::cout << std::fixed << std::setprecision(2) << "Total Salary Owed: $"
             << salary;
   return 0;
 }
